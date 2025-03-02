9b15436d8538025747317cc4bc33a7e723d4b8b5
Valentin MILLET <valentin.millet39@gmail.com>
2025-03-02 14:43:38 +0100

    try to fix cypress on ci

--- e2e/cypress.config.ts
+++ e2e/cypress.config.ts
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:8080", // Your Angular app's URL
+    baseUrl: "http://host.docker.internal:8080", // Your Angular app's URL
     specPattern: "cypress/e2e/**/*.cy.{js,jsx,ts,tsx}", // Adjust spec pattern if needed.
     setupNodeEvents() {
       // implement node event listeners here
